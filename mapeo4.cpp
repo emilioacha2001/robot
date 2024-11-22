b919f645dde2411db83d80a4e2f3da6c191b7cac
Orange Pi <al189304@alumnos.uacj.mx>
2024-11-22 17:05:27 +0000

    Mapeos y obtencion de datos

--- mapeo4.cpp
+++ mapeo4.cpp
@@ -19,30 +19,15 @@ int main() {
     motor1.setSpeed(0); motor2.setSpeed(0);
     auto start = chrono::high_resolution_clock::now();
     auto currentTime = chrono::high_resolution_clock::now();
+    chrono::duration<float> sampleTime(2);
+    ofstream mapeo("graphs/mapeo3_simple.txt");
 
     // Mapeo 1
-    int maxSpeed = 1024;
-    int maxPulses1 = 0;
-    int maxPulses2 = 0;
-    ofstream mapeo1("graphs/mapeo1_carga.txt");
-    chrono::duration<float> sampleTime(1);
-    bool negativeIteration = true;
+    int speeds[9] = {620, 670, 720, 770, 820, 870, 920, 970, 1024};
 
-    for (long speed = 0; speed <= maxSpeed; speed++) {
-        if (speed < 600) {
-            if (negativeIteration) mapeo1 << -speed << ", 0, 0" << endl;
-            else mapeo1 << speed << ", 0, 0" << endl;
-            continue;
-        };
-
-        if (negativeIteration) {
-            motor1.setSpeed(-speed);
-            motor2.setSpeed(-speed);
-        } else {
-            motor1.setSpeed(speed);
-            motor2.setSpeed(speed);
-
-        }
+    for (int i = 0; i < 9; i++) {
+        motor1.setSpeed(speeds[i]);
+        motor2.setSpeed(speeds[i]);
 
         currentTime = chrono::high_resolution_clock::now();
         chrono::duration<float> duration = chrono::high_resolution_clock::now() - currentTime;
@@ -62,55 +47,16 @@ int main() {
         long double average1 = samplePulses1 / sampleCount;
         long double average2 = samplePulses2 / sampleCount;
 
-        if (average1 >= maxPulses1) maxPulses1 = average1;
-        if (average2 >= maxPulses2) maxPulses2 = average2;
+        cerr << "Speed: " << speeds[i] << " Average1: " << average1/2 << " Average2: " << average2/2 << " Time: " << (currentTime - start).count() << endl;
+        mapeo << speeds[i] << ", " << average1/2 << ", " << average2/2 << ", " << (currentTime - start).count() << endl;
 
-        if (negativeIteration) {
-            cerr << " Speed: " << -speed << " Average1: " << average1 << " Average2: " << average2 << " Samples: " << sampleCount << endl;
-            mapeo1 << -speed << ", " << -average1 << ", " << -average2 << endl;
-        } else {
-            cerr << " Speed: " << speed << " Average1: " << average1 << " Average2: " << average2 << " Samples: " << sampleCount << endl;
-            mapeo1 << speed << ", " << average1 << ", " << average2 << endl;
-        }
-
-        if (speed == maxSpeed && negativeIteration) {
-            speed = 0;
-            negativeIteration = false;
-        }
+        motor1.setSpeed(0);
+        motor2.setSpeed(0);
+        usleep(10000000/2);
     }
-    mapeo1.close();
 
-    vector<std::tuple<double, double, double>> data;
-    std::ifstream inputFile("graphs/mapeo1_carga.txt");
-    double speed, average1, average2;
-    std::string line;
-
-    while (std::getline(inputFile, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        double speed, average1, average2;
-
-        // Parse the comma-separated values
-        std::getline(ss, value, ',');
-        speed = std::stod(value);
-        std::getline(ss, value, ',');
-        average1 = std::stod(value);
-        std::getline(ss, value, ',');
-        average2 = std::stod(value);
-
-        data.emplace_back(speed, average1, average2);
-    }
-    inputFile.close();
-
-    std::sort(data.begin(), data.end(), [](auto &a, auto &b) {
-        return std::get<0>(a) < std::get<0>(b);
-    });
-
-    ofstream mapeo1Sorted("graphs/mapeo1_carga.txt");
-    for (auto &row : data) {
-        mapeo1Sorted << std::get<0>(row) << ", " << std::get<1>(row) << ", " << std::get<2>(row) << endl;
-    }
-    mapeo1Sorted.close();
+    motor1.setSpeed(0);
+    motor2.setSpeed(0);
 
     return 0;
 }
--- regresor_inverso.cpp
+++ regresor_inverso.cpp
@@ -27,13 +27,13 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < g; i++) {
         for (int j = 0; j < g; j++)
             A(i, j) = accu(pow(D.col(0), (i + j)));
-        y(i) = accu(D.col(2) % pow(D.col(0), i));
+        y(i) = accu(D.col(1) % pow(D.col(3), i));
     }
 
     coeficientes = inv(A) * y;
     cout << "Coeficientes del regresor: " << coeficientes.t() << endl;
 
-    ofstream outFile("regresores/mapeo3_simple_m2.txt");
+    ofstream outFile("regresores/mapeo3_simple_m1_tiempo.txt");
     if (!outFile) {
         cerr << "Error al abrir el archivo para escritura\n";
         return 1;
@@ -47,7 +47,7 @@ int main(int argc, char *argv[]) {
     }
 
     for (size_t i = 0; i < D.n_rows; i++) {
-        outFile << D(i, 0) << " " << D(i, 2) << " NaN\n"; // Original data
+        outFile << D(i, 3) << " " << D(i, 1) << " NaN\n"; // Original data
     }
     for (size_t i = 0; i < x.n_elem; i++) {
         outFile << x(i) << " NaN " << y_pred(i) << "\n"; // Polynomial fit
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
     cout << "Data written to output.dat\n";
 
     // cpi regresor.cpp "graphs/mapeo1.txt" 3
-    // plot "regresores/mapeo3_simple_m1" using 1:2 with lines title "Original Data" pointtype 7, "regresores/mapeo3_simple_m1" using 1:3 with lines title "Polynomial Fit" linecolor "red"
+    // plot "regresores/mapeo3_simple_m1_tiempo.txt" using 1:2 with lines title "Original Data" pointtype 7, "regresores/mapeo3_simple_m1_tiempo.txt" using 1:3 with lines title "Polynomial Fit" linecolor "red"
     // plot "output2.dat" using 1:2 with lines title "Original Data" pointtype 7, "output2.dat" using 1:3 with lines title "Polynomial Fit" linecolor "red"
 
 
